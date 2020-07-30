7d9f9a7b35dfc051e3e038b313a2ca00fdeb67fd
NormanBenbrahim <norman.benbrahim@gmail.com>
2020-07-30 00:26:00 -0400

    first

---
+++ setup.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+
+import matplotlib.pyplot as plt
+
+print("Version: ", tf.__version__)
+print("Eager mode: ", tf.executing_eagerly())
+print("Hub version: ", hub.__version__)
+print("GPU is", "available" if tf.config.list_physical_devices('GPU') else "NOT AVAILABLE")
+
+# downloads the data to local
+train_data, test_data = tfds.load(name="imdb_reviews", split=["train", "test"], 
+                                  batch_size=-1, as_supervised=True)
+
+train_examples, train_labels = tfds.as_numpy(train_data)
+test_examples, test_labels = tfds.as_numpy(test_data)
